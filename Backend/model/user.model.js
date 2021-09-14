6ccf0ceb97950ec6e5c5358f6d4e6d2a49f840c1
jimboden1 <jimboden1@murraystate.edu>
2021-09-14 17:42:50 -0400

    added attempts to track number of failed login attempts

--- Backend/model/user.model.js
+++ Backend/model/user.model.js
@@ -3,13 +3,15 @@ let mongoose = require("mongoose");
 mongoose.pluralize(null);
 
 let userSchema = mongoose.Schema({
-    email:{type:String,require:true, unique:true},
-    fname:{type:String,require:true},
+    email:{type:String, require:true, unique:true},
+    fname:{type:String, require:true},
     mname:{type:String},
-    lname:{type:String,require:true},
-    birth:{type:Date,require:true},
-    phone:{type:Number,min: [10, 'Please enter the full phone number including area code!']},
-    password:{type:String,require:true, select:false}
+    lname:{type:String, require:true},
+    birth:{type:Date, require:true},
+    attempts:{type:Number},
+    funds:{type:Number, require:true},
+    phone:{type:Number, min: [10, 'Please enter the full phone number including area code!']},
+    password:{type:String, require:true, select:false}
 })
 
 let userModel = mongoose.model("User",userSchema);
