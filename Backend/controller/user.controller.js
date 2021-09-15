1db910c4e4a6bfc551ecc08fcd2de74fddcddaa6
jimboden1 <jimboden1@murraystate.edu>
2021-09-15 13:05:47 -0400

    added login attempts and initial fuds and attempts on sign up

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -10,18 +10,29 @@ let signUp = async (request,response)=>{
     }
     else{
         user._id=last._id+1;//increment the id from last user and assign it
+        user.attempts = 3;
+        user.funds = 100;
         await userModel.insertMany(user);//add user to database
         response.send(`Thank you for signing up your user ID is: ${user._id}`)
     }
 }
 let signIn = async (request,response)=>{
     let user = request.body;
-    let userIndex = await userModel.findOne({_id:user._id,password:user.password});
-    if(userIndex!=null){
+    let userInfo = await userModel.findOne({_id:user._id});
+    let passwordCheck = await userModel.findOne({_id:user._id,password:user.password});
+    if(passwordCheck!=null && userInfo.attempts>0){
+        await userModel.updateOne({_id:user._id},{$set:{attempts:3}});
         response.send("Success");
     }
+    else if(userInfo.attempts==0){
+        response.send(`You have run out of login attempts.
+        Please raise a ticket with our help desk to unlock your account.`)
+    }
     else{
-        response.send("UserID or Password is incorrect!");
+        if(userInfo!=null){
+            await userModel.updateOne({_id:userInfo._id},{$set:{attempts:userInfo.attempts-1}})
+        }
+        response.send(`UserID or Password is incorrect! You have ${userInfo.attempts-1} attempts remaining.`);
     }
 }
 
