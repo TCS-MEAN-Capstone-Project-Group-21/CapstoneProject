ccb0d6c579950c0e92293ea0ab9c51cc3f385c3a
jimboden1 <jimboden1@murraystate.edu>
2021-09-15 22:02:47 -0400

    created Schema and Model

---
+++ Backend/model/order.model.js
@@ -0,0 +1,15 @@
+let mongoose = require("mongoose");
+
+mongoose.pluralize(null);
+
+let orderSchema = mongoose.Schema({
+    _id:{type:Number, require:true},
+    userId:{type:Number, require:true},
+    status:{type:String, require:true},
+    cost:{type:Number, require:true},
+    items:{type:Array, require:true}
+})
+
+let orderModel = mongoose.model("Order",orderSchema);
+
+module.exports = orderModel
---
+++ Backend/model/ticket.model.js
@@ -0,0 +1,14 @@
+let mongoose = require("mongoose");
+
+mongoose.pluralize(null);
+
+let ticketSchema = mongoose.Schema({
+    _id:{type:Number, require:true},
+    userid:{type:Number, require:true},
+    issue:{type:String, require:true},
+
+})
+
+let ticketModel = mongoose.model("Ticket",ticketSchema);
+
+module.exports = ticketModel
