8a405fde55e083d670845daed4c9e2c41b337b1e
kayladc10 <kec1395@gmail.com>
2021-09-16 19:45:35 -0400

    raise ticket edits more

--- Frontend/CapstoneFrontend/src/app/ticket.ts
+++ Frontend/CapstoneFrontend/src/app/ticket.ts
@@ -1,2 +1,15 @@
 export class Ticket {
+    _id?:number;
+    userid?:number; //the name is the id name from the html page of each input/selection
+    issue?:String;
+    status?:string;
+    productprice?:number;
+    productquantity?:number
+
+    // COPIED FROM (ticket.model.js)
+    // _id:{type:Number, require:true},
+    // userid:{type:Number, require:true},
+    // issue:{type:String, require:true},
+    // status:{type:String, require:true}
+    constructor(){}
 }
