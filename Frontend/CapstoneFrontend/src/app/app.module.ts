c8e02c75b4c937fc48cc0f6c006870c75f402be7
Hermes Obiang <h.asumuobiang@wsu.edu>
2021-09-15 20:41:10 -0700

    updated some work on order status, Funds and Edit Profile

--- Frontend/CapstoneFrontend/src/app/app.module.ts
+++ Frontend/CapstoneFrontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FundsComponent } from './user/funds/funds/funds.component';
+import { OrderStatusComponent } from './user/order-status/order-status/order-status.component';
+import { EditProfileComponent } from './user/edit-profile/edit-profile/edit-profile.component';
+import { SignInComponent } from './Employee/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FundsComponent,
+    OrderStatusComponent,
+    EditProfileComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
