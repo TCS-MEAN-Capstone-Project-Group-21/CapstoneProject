fccd634e4476188974a2a8bbc88161cc08644cfe
jimboden1 <jimboden1@murraystate.edu>
2021-09-16 10:31:53 -0400

    added status to tickets

--- Backend/model/ticket.model.js
+++ Backend/model/ticket.model.js
@@ -6,7 +6,7 @@ let ticketSchema = mongoose.Schema({
     _id:{type:Number, require:true},
     userid:{type:Number, require:true},
     issue:{type:String, require:true},
-
+    status:{type:String, require:true}
 })
 
 let ticketModel = mongoose.model("Ticket",ticketSchema);
