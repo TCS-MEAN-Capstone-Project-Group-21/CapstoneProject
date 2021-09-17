4b42a38187817007d36cccc48cee4fd2db5d34c2
jimboden1 <jimboden1@murraystate.edu>
2021-09-17 07:45:13 -0400

    created order query for backend

--- Backend/controller/order.controller.js
+++ Backend/controller/order.controller.js
@@ -2,7 +2,9 @@ let orderModel = require("../model/order.model");
 
 let addOrder = async (request,response)=>{
     let order = request.body
-    order.status="recieved"
+    let date = new Date();
+    order.status="recieved";
+    order.date = date;
     let last = await orderModel.findOne({}).sort({_id:-1});//get last order in database
     if(last!=null){
         order._id=last._id+1;//increment the id from last order and assign it
@@ -12,6 +14,26 @@ let addOrder = async (request,response)=>{
     }
     await orderModel.insertMany(order);
     response.send("Thank you for your Order!")
+}
+
+let findOrder = async (request,response)=>{
+    let query = request.body;
+    let timeEnd = query.date;//set an end query time
+    timeEnd.setHours(23);//Always set end query time to end of day
+    timeEnd.setMinutes(59);
+    timeEnd.setSeconds(59);
+    if(query.type == "weekly"){
+        timeEnd.setDate(query.date.getDate()+6);
+    }
+    else if(query.type == "Monthly"){
+        timeEnd.setMonth(query.date.getMonth()+1);
+    }
+    let result = await orderModel.find({
+        userId:query.userId,//where userId maches entered user Id
+        items:{$in:query.productId},//where product Id is included in the items array
+        date:{$gt:query.date,$lt:timeEnd}//on orders that fall between the start and end date
+    })
+    response.send(result);
 }
 
 module.exports={addOrder};
--- Backend/model/order.model.js
+++ Backend/model/order.model.js
@@ -6,8 +6,9 @@ let orderSchema = mongoose.Schema({
     _id:{type:Number, require:true},
     userId:{type:Number, require:true},
     status:{type:String, require:true},
+    date:{type:Date, require:true},
     cost:{type:Number, require:true},
-    itemid:{type:Number, require:true}
+    items:{type:Array, require:true}
 })
 
 let orderModel = mongoose.model("Order",orderSchema);
