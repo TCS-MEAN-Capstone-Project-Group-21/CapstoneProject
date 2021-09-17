721b0b21af3ebc2c856a80a37ac1453c5202ef65
jimboden1 <jimboden1@murraystate.edu>
2021-09-16 20:31:45 -0400

    Update order.controller.js

--- Backend/controller/order.controller.js
+++ Backend/controller/order.controller.js
@@ -5,10 +5,10 @@ let addOrder = async (request,response)=>{
     order.status="recieved"
     let last = await orderModel.findOne({}).sort({_id:-1});//get last order in database
     if(last!=null){
-        order._id=last._id+1;//increment the id from last user and assign it
+        order._id=last._id+1;//increment the id from last order and assign it
     }
     else{
-        order._id=0
+        order._id=0//or start count at zero if Database is empty
     }
     await orderModel.insertMany(order);
     response.send("Thank you for your Order!")
