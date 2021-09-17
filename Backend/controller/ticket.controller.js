4cd7c5cbcc4b2631798bcc7cae11baf103f04c55
jimboden1 <jimboden1@murraystate.edu>
2021-09-16 20:50:17 -0400

    created filter for getTicket and created resolveTicket funtion

--- Backend/controller/ticket.controller.js
+++ Backend/controller/ticket.controller.js
@@ -4,14 +4,28 @@ let ticketModel = require("../model/ticket.model");
 
 let raiseticket = ((request,response)=> {
     let ticket = request.body;
+    ticket.status = "active";
+    let last = await ticketModel.findOne({}).sort({_id:-1});//get last ticket in database
+    if(last!=null){
+        order._id=last._id+1;//increment the id from last ticket and assign it
+    }
+    else{
+        order._id=0
+    }
     await ticketModel.insertMany(ticket);//insert ticket into Database
     response.send("Your ticket has been submitted!");
 })
 
 //get all the tickets
 let getTickets = async (request, response)=>{
-    let tickets = await ticketModel.find();
+    let tickets = await ticketModel.find({status:"active"});
     response.send(tickets);
 }
 
-module.exports = {raiseticket,getTickets}
+let resolveTicket = async (request, response)=>{
+    let ticket = request.body;
+    await ticketModel.updateOne({_id:ticket._id},{$set:{status:"Resolved"}})
+    response.send(`Ticket ${ticket._id} has been closed!`)
+}
+
+module.exports = {raiseticket,getTickets,resolveTicket}
